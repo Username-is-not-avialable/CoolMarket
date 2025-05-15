6ee33ad5e8dc6786464fd6cf1ebd729dedf3ba1c
Username-is-not-avialable <antonbakir280@mail.ru>
2025-05-15 18:59:26 +0500

    add DB startup

---
+++ app/services/db.py
@@ -0,0 +1,30 @@
+from contextlib import asynccontextmanager
+from tortoise import Tortoise
+from dotenv import load_dotenv  # Добавьте эту строку
+from os import getenv
+
+load_dotenv()
+
+user = getenv("DB_USER")
+password = getenv("DB_PASSWORD")
+async def init_db():
+    await Tortoise.init(
+        db_url=f"postgres://{user}:{password}@localhost:5432/market",
+        modules={
+            "models": [
+                "app.models.user",
+                "app.models.order",
+                "app.models.instrument"
+            ]
+        }
+    )
+    await Tortoise.generate_schemas()
+
+async def close_db():
+    await Tortoise.close_connections()
+
+@asynccontextmanager
+async def lifespan(app: 'FastAPI'):
+    await init_db()
+    yield
+    await close_db()
