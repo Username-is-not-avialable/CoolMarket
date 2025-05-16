3fd44f68b15b8f1e3257df7f4dfa41fd3c344435
Username-is-not-avialable <antonbakir280@mail.ru>
2025-05-16 08:34:52 +0500

    fix user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,13 @@
 from tortoise.models import Model
 from tortoise import fields
 import uuid
+from app.services.api_key import generate_api_key
 
 class User(Model):
-    id = fields.IntField(pk=True)
+    id = fields.UUIDField(pk=True, default = uuid.uuid4)
     name = fields.CharField(max_length=255)
-    api_key = fields.CharField(max_length=36, unique=True, default=lambda: uuid.uuid4())
-    role = fields.CharField(max_length=20, default="user")  # user/admin
+    api_key = fields.CharField(max_length=36, unique=True, default = generate_api_key)
+    role = fields.CharField(max_length=20, default="USER")  # USER/ADMIN
 
     class Meta:
         table = "users"
