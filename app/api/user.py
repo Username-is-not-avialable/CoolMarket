74250f126f50aa446c5bfe0a182205013684af5a
Username-is-not-avialable <antonbakir280@mail.ru>
2025-05-16 08:56:17 +0500

    comment unused modules

--- app/api/user.py
+++ app/api/user.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, Header
-from app.models import User
-from app.services.auth import get_user_by_token
+# from fastapi import APIRouter, Depends, Header
+# from app.models import User
+# from app.services.auth import get_user_by_token
 
 
-router = APIRouter(prefix="/api/v1", tags=["User"])
+# router = APIRouter(prefix="/api/v1", tags=["User"])
 
-@router.get("/balance")
-async def get_balances(user: User = Depends(get_user_by_token)):
-    return {"RUB": 1000}
+# @router.get("/balance")
+# async def get_balances(user: User = Depends(get_user_by_token)):
+#     return {"RUB": 1000}
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from app.services.db import lifespan
-from app.api import public, user, admin
+from app.api import public
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(public.router)
-app.include_router(user.router)
-app.include_router(admin.router)
